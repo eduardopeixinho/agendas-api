073a06c466961e713b5c6e89b909c6e7eeee138c
Eduardo da Silva Peixinho <eduardopeixinho@gmail.com>
2025-03-14 19:47:24 -0300

    - Ajuste para porta do branch master

--- artefatos/agendas.py
+++ artefatos/agendas.py
@@ -351,6 +351,6 @@ if __name__ == '__main__':
     create_table()
 
 # Iniciar a aplicação    
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 # FimS
